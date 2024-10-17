db312c0c2ab07c0056aa3e03fd05e00c3f066e09
Demo <nguyet11042003@gmail.com>
2024-10-18 01:38:27 +0700

    Add file

---
+++ test_py.py
@@ -0,0 +1,13 @@
+msg = "This is question"
+print(msg)
+
+# Define two numbers
+num1 = 10
+num2 = 20
+
+# Perform the addition
+result = num1 + num2
+
+# Print the result
+print("The result of the addition is:", result)
+
